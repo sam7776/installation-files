eeb16d78253cd4edf191b7424a3471a3cfb229a0
sam7776 <43927210+sam7776@users.noreply.github.com>
2025-04-18 00:09:52 +0530

    updated files are here
    lo ji ji lo zindagi maje me

---
+++ jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+java --version && jenkins --version
